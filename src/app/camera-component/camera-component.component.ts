deffe76999e81743d27e8f4e37cfcee60420b317
lcharanteja <teja.charan20@gmail.com>
2019-10-20 21:18:45 -0400

    Access web cam 📷

--- src/app/camera-component/camera-component.component.ts
+++ src/app/camera-component/camera-component.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-camera-component',
-  templateUrl: './camera-component.component.html',
-  styleUrls: ['./camera-component.component.css']
+  selector: "app-camera-component",
+  templateUrl: "./camera-component.component.html",
+  styleUrls: ["./camera-component.component.css"]
 })
 export class CameraComponentComponent implements OnInit {
+  @ViewChild('video', { static: true }) video;
+  videoConstraints = { 
+    audio: true, 
+    video: { facingMode: { exact: "environment" }}};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const videoElement: HTMLVideoElement = this.video.nativeElement;
 
+    navigator.mediaDevices
+      .getUserMedia({
+      video: { facingMode: 'user' }
+    })
+      .then((stream) => {
+        /* use the stream */
+        videoElement.srcObject = stream;
+      })
+      .catch((err) => {
+        /* handle the error */
+      });
+  }
 }
