b205f75f7f0c482266ba7126f98cb5445a1b2619
lcharanteja <teja.charan20@gmail.com>
2019-10-21 05:07:19 -0400

    added few css and camera record logic.

--- src/app/camera-component/camera-component.component.ts
+++ src/app/camera-component/camera-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
+import { Observable, from, timer, Subject, race } from "rxjs";
+import { tap, map } from 'rxjs/operators';
+
+declare var MediaRecorder: any;
 
 @Component({
   selector: "app-camera-component",
@@ -6,26 +10,72 @@ import { Component, OnInit, ViewChild } from "@angular/core";
   styleUrls: ["./camera-component.component.css"]
 })
 export class CameraComponentComponent implements OnInit {
-  @ViewChild('video', { static: true }) video;
-  videoConstraints = { 
-    audio: true, 
-    video: { facingMode: { exact: "environment" }}};
+  @ViewChild("preview", { static: true }) video;
+  previewElement: HTMLVideoElement;
+  videoConstraints = {
+    audio: true,
+    video: { facingMode: { exact: "environment" } }
+  };
+
+  mediaRecorder: any;
+  recordedChunks = [];
+  lengthInMS = 10000;
+
+  show = false;
+
+  mediaObservable$: Observable<MediaStream>;
+  timeOut$ = timer(this.lengthInMS);
+  stopped$ = new Subject<void>()
+  
+
+  
 
   constructor() {}
 
   ngOnInit() {
-    const videoElement: HTMLVideoElement = this.video.nativeElement;
+    this.previewElement = this.video.nativeElement;
 
-    navigator.mediaDevices
-      .getUserMedia({
-      video: { facingMode: 'user' }
-    })
-      .then((stream) => {
-        /* use the stream */
-        videoElement.srcObject = stream;
+    this.mediaObservable$ = from(
+      navigator.mediaDevices.getUserMedia({
+        video: { facingMode: "user", width: 1280, height: 720 }
       })
-      .catch((err) => {
-        /* handle the error */
-      });
+    );
+
+    this.startCamera();
   }
+
+  startCamera = () => {
+    this.mediaObservable$.subscribe(
+      stream => {
+        /* use the stream */
+        this.previewElement.srcObject = stream;
+        this.mediaRecorder = new MediaRecorder(stream);
+      },
+      error => console.log('This error' + error)
+    );
+  }
+
+  record() {
+    this.show = true;
+    this.mediaRecorder.start();
+    console.log(this.mediaRecorder.state);
+    console.log("recorder started");
+
+    // push data to array
+    this.mediaRecorder.ondataavailable = event => this.recordedChunks.push(event.data);
+
+    this.mediaRecorder.start();
+    console.log(this.mediaRecorder.state + " for " + this.lengthInMS / 1000 + " seconds...");
+
+    race(this.timeOut$, this.stopped$).subscribe(
+      stop => this.mediaRecorder.stop()
+    );
+  }
+  stop() {
+    
+    console.log(this.mediaRecorder.state);
+    console.log("recorder stopped");
+    this.stopped$.next();
+  }
+
 }
