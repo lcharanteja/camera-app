7db74ae8175c375a17f3b8f4eb780509ef5615fa
lcharanteja <teja.charan20@gmail.com>
2019-10-20 20:58:12 -0400

    Generated Camera component. Added Background color as beige🎨

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CameraComponentComponent } from './camera-component/camera-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CameraComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/camera-component/camera-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-component',
+  templateUrl: './camera-component.component.html',
+  styleUrls: ['./camera-component.component.css']
+})
+export class CameraComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
